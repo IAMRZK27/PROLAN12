1ca5883ad14900c619e9d842ed8809cf3a7753eb
Ilham Rizki H <ilham.rizki.h97@gmail.com>
2017-02-28 18:29:11 +0700

    Tugas Minggu_2

---
+++ Minggu_2/Mahasiswa.java
@@ -0,0 +1,8 @@
+//program hello world
+class Mahasiswa{
+	public static void main(String argv[]){
+		System.out.println("NAMA   : ILHAM RIZKI HIDAYATULLOH");
+		System.out.println("NIM    : 10115364");
+		System.out.println("ALAMAT : Jl. Dr. Junjunan Dalam III");
+		}
+	}
