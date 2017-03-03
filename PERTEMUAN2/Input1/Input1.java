87e2f1de0c706f424478b9e3559dd6d82b281a5b
Ilham Rizki H <ilham.rizki.h97@gmail.com>
2017-03-03 14:31:48 +0700

    Praktikum Lab Prolan-12(3)

---
+++ PERTEMUAN2/Input1/Input1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Input1{
+	public static void main(String args[]) {
+        Scanner namaVariabel = new Scanner(System.in);
+        String nama;
+        nama = namaVariabel.nextLine();
+    }
+}
